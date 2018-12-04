d3ac6340776f8a0773318a0d86913ecc89b02103
dowolken <45375823+dowolken@users.noreply.github.com>
2018-12-04 09:22:58 +0800

    Update pastpaper 2015.java

--- 20181204-Cloud/pastpaper 2015.java
+++ 20181204-Cloud/pastpaper 2015.java
@@ -1,8 +1,10 @@
 //pastpaper 2015 for practicing string and class
 public class HiddenWord{
   private String str;
+  
   public HiddenWord(String a){
     str=a;}
+  
   public String getHint(String a){
     String hidden="";
     for(int i=0;i<a.length();i++){
