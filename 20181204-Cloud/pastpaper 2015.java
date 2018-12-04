12ab52a31e67292ab0ab5a1830fdc4ebeec0cb03
dowolken <45375823+dowolken@users.noreply.github.com>
2018-12-04 09:37:02 +0800

    Update pastpaper 2015.java

--- 20181204-Cloud/pastpaper 2015.java
+++ 20181204-Cloud/pastpaper 2015.java
@@ -1,16 +1,16 @@
 //pastpaper 2015 for practicing string and class
 public class HiddenWord{
-  private String str;
+  private String puzzle;
   
   public HiddenWord(String a){
-    str=a;}
+    puzzle=a;}
   
   public String getHint(String a){
     String hidden="";
     for(int i=0;i<a.length();i++){
-      if((str.substring(i,i+1)).equals.(a.substring(i,i+1)))
-      {hidden+=str.substring(i,i+1));}
-      else(if(!str.indexOf(a.substring(i,i+1)))
+      if((puzzle.substring(i,i+1)).equals.(a.substring(i,i+1)))
+      {hidden+=puzzle.substring(i,i+1));}
+      else(if(!puzzle.indexOf(a.substring(i,i+1)))
            {hidden+="*";}
            else
            {hidden+="+";}
