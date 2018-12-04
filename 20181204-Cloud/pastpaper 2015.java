95e622ac1f1546ae1a8ee392d09cf949e1d39b3b
dowolken <45375823+dowolken@users.noreply.github.com>
2018-12-04 09:28:35 +0800

    Revert "20181204 cloud"

--- 20181204-Cloud/pastpaper 2015.java
+++ 20181204-Cloud/pastpaper 2015.java
@@ -1,18 +1,1 @@
 //pastpaper 2015 for practicing string and class
-public class HiddenWord{
-  private String str;
-  
-  public HiddenWord(String a){
-    str=a;}
-  
-  public String getHint(String a){
-    String hidden="";
-    for(int i=0;i<a.length();i++){
-      if((str.substring(i,i+1)).equals.(a.substring(i,i+1)))
-      {hidden+=str.substring(i,i+1));}
-      else(if(!str.indexOf(a.substring(i,i+1)))
-           {hidden+="*";}
-           else
-           {hidden+="+";}
-           }
-     return hidden;
